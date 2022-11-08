eda397a1928b1fb5014b98f0362d0de016f33d94
tiabaik <106868597+tiabaik@users.noreply.github.com>
2022-11-08 20:10:32 +0700

    GD10_D_10653_andro

---
+++ app/src/main/java/com/tia_0653/gd10_d_10653/ResponseCreate.kt
@@ -0,0 +1,9 @@
+package com.tia_0653.gd10_d_10653
+
+import com.google.gson.annotations.SerializedName
+
+class ResponseCreate (
+    @SerializedName("status") val stt:Int,
+    @SerializedName("error") val e:Boolean,
+    @SerializedName("message") val pesan:String,
+)
---
+++ app/src/main/java/com/tia_0653/gd10_d_10653/ResponseDataMahasiswa.kt
@@ -0,0 +1,8 @@
+package com.tia_0653.gd10_d_10653
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDataMahasiswa(
+    @SerializedName("status") val stt:String,
+    val totaldata: Int,
+    val data:List<MahasiswaData>)
